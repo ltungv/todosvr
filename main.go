fe576ef169837a7099ce2233ae81388ffb67c315
letung3105 <15053897+letung3105@users.noreply.github.com>
2020-10-02 10:15:30 +0700

    [DOCS] Swagger

--- main.go
+++ main.go
@@ -8,9 +8,12 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/go-chi/chi"
 	"github.com/go-sql-driver/mysql"
+	"github.com/letung3105/todos-svr/docs"
 	"github.com/letung3105/todos-svr/service"
 	"github.com/letung3105/todos-svr/storage"
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
 var (
@@ -21,12 +24,25 @@ var (
 
 func init() {
 	flag.StringVar(&dsn, "dsn", "@/todosvr", "The connection string of the database")
-	flag.StringVar(&host, "host", "", "Host address to bind to")
+	flag.StringVar(&host, "host", "localhost", "Host address to bind to")
 	flag.IntVar(&port, "port", 3000, "Port number to bind to")
 }
 
+// @contact.name Tung L. Vo
+// @contact.url https://github.com/letung3105
+// @contact.email letung3105@gmail.com
+// @license.name MIT
+// @license.url https://mit-license.org
+
 func main() {
 	flag.Parse()
+
+	docs.SwaggerInfo.Title = "Todo API"
+	docs.SwaggerInfo.Description = "Dịch vụ API đơn giản"
+	docs.SwaggerInfo.Version = "0.1.0"
+	docs.SwaggerInfo.Host = fmt.Sprintf("%s:%d", host, port)
+	docs.SwaggerInfo.BasePath = "/"
+	docs.SwaggerInfo.Schemes = []string{"http"}
 
 	// TODO: cài đặt cơ sở dữ liệu thông qua cấu hình tuỳ chỉnh
 	cfg, err := mysql.ParseDSN(dsn)
@@ -48,9 +64,17 @@ func main() {
 		log.Fatal(err)
 	}
 
+	r := chi.NewRouter()
+	r.Get("/swagger/*", httpSwagger.Handler(
+		httpSwagger.URL("http://localhost:3000/swagger/doc.json"),
+		// ^ NOTE: đường dẫn tới tệp tin JSON chứa định nghĩa của API,
+		// thông tin của tệp tin này được tạo bởi package docs
+	))
+	r.Mount("/", service.NewTodo(storage.NewTodo(db)))
+
 	svr := http.Server{
 		Addr:         fmt.Sprintf("%s:%d", host, port),
-		Handler:      service.NewTodo(storage.NewTodo(db)),
+		Handler:      r,
 		TLSConfig:    nil,
 		WriteTimeout: time.Second, // thời gian tối đa được dùng để ghi gói tin
 		ReadTimeout:  time.Second, // thời gian tối đa được dùng để đọc gói tin
--- service/todo.go
+++ service/todo.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"context"
 	"encoding/json"
 	"log"
 	"net/http"
@@ -13,7 +14,6 @@ import (
 )
 
 // Todo chứa các hàm số dùng để khởi tạo và chạy dịch vụ API.
-// TODO: thêm database để  lưu trữ dữ liệu người dùng
 type Todo struct {
 	router  chi.Router
 	storage *storage.Todo
@@ -38,22 +38,36 @@ func (todo *Todo) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 // routes cài đặt các đường dẫn HTTP được hỗ trợ bởi dịch vụ.
 // TODO: thêm handlers để xử lý thao tác CRUD
 func (todo *Todo) routes() {
+	notImpl := func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(SimpleMsgResponse{"NOT IMPLEMENTED"})
+	}
+
 	todo.router.Use(
 		middleware.JSONResponse(),
 		middleware.LogRequest(log.New(os.Stdout, "", log.LstdFlags)),
 	)
-
-	// TODO: xử lý PUT, GET và DELETE cho đường dẫn /todo
 	todo.router.Get("/hello", todo.GetHello())
 	todo.router.Post("/todo", todo.CreateOneTask())
+	todo.router.Get("/todo", notImpl)
+	todo.router.Route("/todo/{id}", func(r chi.Router) {
+		r.Use(TaskIDCtx())
+		r.Get("/", notImpl)
+		r.Put("/", notImpl)
+		r.Delete("/", notImpl)
+	})
 }
 
 // CreateOneTask lấy thông tin của một tác vụ từ body của request rồi gửi cho storage.Todo để xứ lý.
+// @Summary Create one task
+// @Description tạo một tác vụ mới
+// @Accept json
+// @Produce json
+// @Param task body storage.Task true "tác vụ được thêm vào"
+// @Failure 500 {object} service.ErrResponse
+// @Failure 400 {object} service.ErrResponse
+// @Success 200 {array} storage.Task
+// @Router /todo [post]
 func (todo *Todo) CreateOneTask() http.HandlerFunc {
-	type errResponse struct {
-		Err string `json:"error"`
-	}
-
 	// NOTE: chúng ta không cần kiểm tra lỗi trả về từ hàm số `Encode` vì:
 	// + các struct không chứa các kiểu dữ liệu không được hỗ trợ bởi JSON
 	// + các struct không phải là cyclic data structure
@@ -63,18 +77,16 @@ func (todo *Todo) CreateOneTask() http.HandlerFunc {
 		if err := json.NewDecoder(r.Body).Decode(&task); err != nil {
 			log.Println(err)
 			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(errResponse{err.Error()})
+			json.NewEncoder(w).Encode(ErrResponse{err.Error()})
 			return
 		}
-
 		// TODO: kiểm tra thông tin của `task`
-		// TODO: giải thích thư viện "context"
 
 		newTask, err := todo.storage.CreateOne(r.Context(), task)
 		if err != nil {
 			log.Println(err)
 			w.WriteHeader(http.StatusInternalServerError)
-			json.NewEncoder(w).Encode(errResponse{err.Error()})
+			json.NewEncoder(w).Encode(ErrResponse{err.Error()})
 			return
 		}
 
@@ -83,16 +95,42 @@ func (todo *Todo) CreateOneTask() http.HandlerFunc {
 	})
 }
 
-// GetHello trả về một handler có thể xử lý yêu cầu HTTP và trả về chuỗi ký tự "Hello, world." nằm trong một object JSON.
+// GetHello trả về một handler có thể xử lý yêu cầu HTTP và trả về chuỗi ký tự "Hello, world."
+// @Summary Hello world
+// @Description trả về chuỗi kí tự "Hello World"
+// @Produce json
+// @Success 200 {object} service.SimpleMsgResponse
+// @Router /todo [post]
 func (todo *Todo) GetHello() http.HandlerFunc {
-	type response struct {
-		Message string `json:"message"`
-	}
-
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if err := json.NewEncoder(w).Encode(response{quote.HelloV3()}); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
+		json.NewEncoder(w).Encode(SimpleMsgResponse{quote.HelloV3()})
 	})
+}
+
+// ctxKey là kiểu dữ liệu được dùng để chỉ đến các giá trị được lưu trong request context,
+// do bất cứ package nào cũng có thể sử dụng request context nên chúng ta phải có một kiểu
+// dữ liệu riêng cho mỗi package để tránh việc các package ghi đè lên thông tin của nhau
+type ctxKey string
+
+// TaskIDCtxKey tên của giá trị nằm trong request context
+var TaskIDCtxKey = ctxKey("TaskIDCtxKey")
+
+// TaskIDCtx lấy id của task từ đường dẫn url
+func TaskIDCtx() middleware.Middleware {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := context.WithValue(r.Context(), TaskIDCtxKey, chi.URLParam(r, "id"))
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
+
+// SimpleMsgResponse chứa một tin nhắn đơn giản được trả về từ server
+type SimpleMsgResponse struct {
+	Message string `json:"message"`
+}
+
+// ErrResponse chứa chuỗi kí tự miêu tả lỗi đã xảy ra
+type ErrResponse struct {
+	Err string `json:"error"`
 }
